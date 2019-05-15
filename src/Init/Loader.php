5af212a9c14d143728f647682a8d8df5122f4ed5
Ghupeng <ghupeng@163.com>
2019-05-15 17:42:35 +0800

    add loader

--- src/Init/Loader.php
+++ src/Init/Loader.php
@@ -6,17 +6,21 @@
  * Time: 18:06:15
  * By: Loader.php
  */
+namespace Yok\Init;
 
-$loader = new \Phalcon\Loader();
+class Loader {
 
-/**
- * We're a registering a set of directories taken from the configuration file
- */
+	public static function init(&$loader,$config) {
 
-$loader->registerDirs(
-	[
-		APP_PATH . $config->application->controllersDir,
-		APP_PATH . $config->application->modelsDir
-	]
-)->register();
+		/**
+		 * We're a registering a set of directories taken from the configuration file
+		 */
 
+		$loader->registerDirs(
+			[
+				BASE_PATH . $config->controllersDir,
+				BASE_PATH . $config->modelsDir
+			]
+		)->register();
+	}
+}
