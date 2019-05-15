f8e97b418cef1bc8641e115fe3c830b7d7fa8124
Ghupeng <ghupeng@163.com>
2019-05-15 17:42:55 +0800

    add config library

--- src/Library/ConfigLibrary.php
+++ src/Library/ConfigLibrary.php
@@ -2,44 +2,80 @@
 /**
  * Created by vim.
  * User: huguopeng
- * Date: 2019/05/13
- * Time: 18:06:39
- * By: ConfigLibrary.php
+ * Date: 2017-05-20
+ * Time: 16:27
  */
+
 namespace Yok\Library;
 
-class ConfigLibrary {
-    /**
-     * @$param string $filename 文件名
-     * @$param string $module   模块
-     * @$param string $key      键
-     */
-    public static function get(string $filename,string $module,string $key='') {
-        $runmode = \Yok\Library\Runmode::get();
-        $filename = CONF_PATH . '/' . $runmode . '/' . $filename . '.ini';
-        $config = self::getConfigFile($filename,$module);
-        if($config === null) {
-            echo "文件不存在";die;
-        } else if(isset($config[$key])){
-            return $config[$key];
-        } else {
-            return $config;
-        }
-    }
-    /**
-     * @param string $filename
-     * @param string $module
-     * @return null
-     */
-    public static function getConfigFile(string $filename='',string $module='') {
-        if(!file_exists($filename)) {
-            return null;
-        }
-        $config = new \Phalcon\Config\Adapter\Ini($filename);
-        if(empty($config) || !isset($config->$module)) {
-            return null;
-        } else {
-            return $config->$module;
-        }
-    }
+class ConfigLibrary
+{
+	/**
+	 * @param string $sid
+	 * @param string $module
+	 * @param string|null $key
+	 * @return null
+	 */
+	public static function get(string $sid, string $module, string $key=null){
+		$ruMode = Runmode::get();
+		$configFile = CONFIG_PATH."/$ruMode/$sid.ini";
+		$configModule = self::getFromConfigFile( $configFile, $module);
+		if( $configModule == null ) {
+			$configFile = CONFIG_PATH."/share/$sid.ini";
+			$configModule = self::getFromConfigFile( $configFile, $module);
+		}
+
+		if( $key == null) {
+			return $configModule;
+		}else {
+			return $configModule->$key;
+		}
+	}
+
+	/**
+	 * @param string $configFile
+	 * @param string $module
+	 * @return null
+	 * @throws BaseException
+	 */
+	public static function getFromConfigFile(string $configFile, string $module) {
+		if( !file_exists($configFile)) {
+			return null;
+		}
+		try{
+			$iniReader = new \Phalcon\Config\Adapter\Ini($configFile);
+			if( $iniReader == null || !isset($iniReader->$module)) {
+				return null;
+			}else {
+				return $iniReader->$module;
+			}
+		}catch (\Exception $e){
+			echo "异常".$e->getMessage();die;
+		}
+	}
+
+	/**
+	 * @param string $configFile
+	 * @param string $module
+	 * @param string $key
+	 * @return null
+	 * @throws BaseException
+	 */
+	public static function geFromConfigFileByKey(string $configFile, string $module, string $key){
+		if( !file_exists($configFile)) {
+			echo "null";die;
+			return null;
+		}
+
+		try{
+			$iniReader = new \Phalcon\Config\Adapter\Ini($configFile);
+			if( $iniReader == null || !isset($iniReader->$module) || !isset($iniReader->$module->$key) ) {
+				return null;
+			}else {
+				return $iniReader->$module->$key;
+			}
+		}catch (\Exception $e){
+			echo "异常".$e->getMessage();die;
+		}
+	}
 }
