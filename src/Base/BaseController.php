cd55c9a01d763b3984a50d345e31f4a08fea43b5
Ghupeng <ghupeng@163.com>
2019-05-17 15:51:47 +0800

    test

--- src/Base/BaseController.php
+++ src/Base/BaseController.php
@@ -9,9 +9,24 @@
 namespace Yok\Base;
 
 class BaseController extends \Phalcon\Mvc\Controller {
-	
-	public function exectue() {
-		
-	}
-}
 
+	public function exectue() {
+		$basePageInfo = (\Phalcon\DI::getDefault())->get('basePageInfo');
+		$module = ucfirst($basePageInfo->module);
+		$method = ucfirst($basePageInfo->method);
+		$className = PJS_NAMESPACE."\\Models\\".$module."\\Service\\".$method;
+		if(class_exists($className)) {
+			$serviceIns = new $className();
+			$data = $serviceIns->exectue();
+		} else {
+			echo "调用文件不存在";die;
+		}
+		if($basePageInfo->format === 'json'){
+			echo json_encode($data,JSON_UNESCAPED_UNICODE);
+			die;
+		} else {
+			$this->view->render(lcfirst($module), lcfirst($method), $data);
+		}
+	}
+
+}
