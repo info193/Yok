31add58fdcabb01b593dee202981366a6b46b4a2
Ghupeng <ghupeng@163.com>
2019-05-17 17:15:45 +0800

    add form filter

--- src/Plugin/FormFilterPlugin.php
+++ src/Plugin/FormFilterPlugin.php
@@ -13,7 +13,22 @@ use Phalcon\Mvc\Dispatcher;
 use Phalcon\Mvc\User\Plugin;
 class FormFilterPlugin extends Plugin {
 	public function beforeExecuteRoute(Event $event, Dispatcher $dispatcher){
-		
+		$di = \Phalcon\DI::getDefault();
+		$basePageInfo = $di->get('basePageInfo');
+		if($basePageInfo->formCheck === false) {
+			return true;
+		}
+		$module = ucfirst($basePageInfo->module);
+		$method = ucfirst($basePageInfo->method);
+		$className = PJS_NAMESPACE."\\Models\\".$module."\\Param\\".$method."Param";
+		if(class_exists($className)) {
+			$classIns = new $className();
+			$classIns->vaild($classIns,$di,$basePageInfo);
+			$basePageInfo->params = $classIns;
+		} else {
+			echo "参数校验文件未找到";die;
+		}
+		print_r($basePageInfo);die;
 	}
 }
 
