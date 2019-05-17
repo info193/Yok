af23d50e03fd67897ed2e00759447e5ad2914a26
Ghupeng <ghupeng@163.com>
2019-05-17 11:30:18 +0800

    add

--- src/Base/BasePageInfo.php
+++ src/Base/BasePageInfo.php
@@ -9,6 +9,14 @@
 namespace Yok\Base;
 
 class BasePageInfo {
-
+	public $requestType = 'get';
+	public $format  = "json";
+	public $runMode = 'online';
+	public $login   = true;
+	public $formCheck = true;
+	public $module;
+	public $method;
+	public $sessionInfo;
+	public $params;
 }
 
