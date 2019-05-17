6d227d35d68e608a3e20bba25b10cff7041faf27
Ghupeng <ghupeng@163.com>
2019-05-17 11:26:06 +0800

    add form filter

---
+++ src/Plugin/FormFilterPlugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by vim.
+ * User: huguopeng
+ * Date: 2019/05/17
+ * Time: 11:11:10
+ * By: FormFilterPlugin.php
+ */
+namespace Yok\Plugin;
+
+use Phalcon\Events\Event;
+use Phalcon\Mvc\Dispatcher;
+use Phalcon\Mvc\User\Plugin;
+class FormFilterPlugin extends Plugin {
+	public function beforeExecuteRoute(Event $event, Dispatcher $dispatcher){
+		
+	}
+}
+
