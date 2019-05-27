834eb52010b94930fa3ff25c33a3a70167239c87
Ghupeng <ghupeng@163.com>
2019-05-27 16:32:32 +0800

    add ip

--- src/Base/BasePageInfo.php
+++ src/Base/BasePageInfo.php
@@ -12,6 +12,7 @@ class BasePageInfo {
 	public $requestType = 'get';
 	public $format  = "json";
 	public $runMode = 'online';
+	public $requestIP = '0.0.0.0';
 	public $login   = true;
 	public $formCheck = true;
 	public $debug   = false;
