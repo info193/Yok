ae80c5470c217ab771ec57d444f68652953c1051
Ghupeng <ghupeng@163.com>
2019-05-28 15:56:36 +0800

    fix

--- src/Init/Service.php
+++ src/Init/Service.php
@@ -41,98 +41,93 @@ class Service{
 						]);
 
 						return $volt;
-				},'.phtml' => PhpEngine::class]);
+						},'.phtml' => PhpEngine::class]);
 
 				return $view;
+				});
+
+		// Define your routes here
+		$di->setShared('router',function() use ($di){
+				// Use the annotations router. We're passing false as we don't want the router to add its default patterns
+			$router = new RouterAnnotations(false);
+			$routerConfigs = explode(',',ConfigLibrary::get('config','router','list'));
+			// Read the annotations from ProductsController if the URI starts with /*
+			foreach($routerConfigs as $module) {
+				$router->addResource( ucfirst($module), '/' . lcfirst($module) );
+			}
+			return $router;
 		});
+
 
 		/**
 		 * Setting up the dispatch
 		 */
 		$di->set('dispatcher',function () {
-				$lists = explode(',',ConfigLibrary::get('config','plugin','list'));
-				if(empty($lists)){
-					echo "插件有误"; die;
+			$lists = explode(',',ConfigLibrary::get('config','plugin','list'));
+			// Create an event manager
+			$eventsManager = new EventsManager();
+			foreach($lists as $list) {
+				$pluginArr[] = explode('|',$list);
+			}
+			foreach($pluginArr as $plugins) {
+				$pluginCnt = explode('_',$plugins[1]);
+				if(count($pluginCnt) == 2 && $pluginCnt[0] == 'SYS'){
+					$pluginName = "\\Yok\\Plugin\\".$pluginCnt[1];
+				} else {
+					$pluginName = "\\".PJS_NAMESPACE."\\".PLUGIN_NAME."\\".$plugins[1];
 				}
-				// Create an event manager
-				$eventsManager = new EventsManager();
-				foreach($lists as $list) {
-					$pluginArr[] = explode('|',$list);
-				}
-				foreach($pluginArr as $plugins) {
-					$pluginCnt = explode('_',$plugins[1]);
-					if(count($pluginCnt) == 2 && $pluginCnt[0] == 'SYS'){
-						$pluginName = "\\Yok\\Plugin\\".$pluginCnt[1];
+
+				// Attach a listener for type 'dispatch'
+				if( class_exists($pluginName)) {
+					if(!empty($plugins[0])){
+						$dispatcherAttach = 'dispatch:'.$plugins[0];
 					} else {
-						$pluginName = "\\".PJS_NAMESPACE."\\".PLUGIN_NAME."\\".$plugins[1];
+						$dispatcherAttach = 'dispatch';
 					}
-
-					// Attach a listener for type 'dispatch'
-					if( class_exists($pluginName)) {
-						if(!empty($plugins[0])){
-							$dispatcherAttach = 'dispatch:'.$plugins[0];
-						} else {
-							$dispatcherAttach = 'dispatch';
-						}
-						$eventsManager->attach($dispatcherAttach, new $pluginName());
-					}
+					$eventsManager->attach($dispatcherAttach, new $pluginName());
 				}
-				$dispatcher = new MvcDispatcher();
-
-				// Bind the eventsManager to the view component
-				$dispatcher->setEventsManager($eventsManager);
-
-				return $dispatcher;
+			}
+			$dispatcher = new MvcDispatcher();
+			// Bind the eventsManager to the view component
+			$dispatcher->setEventsManager($eventsManager);
+			return $dispatcher;
 		},true);
 
-		// Define your routes here
-		$di->setShared('route',function(){
-				// Use the annotations router. We're passing false as we don't want the router to add its default patterns
-				$router = new RouterAnnotations(false);
-				$routerConfigs = explode(',',ConfigLibrary::get('config','route','list'));
-				// Read the annotations from ProductsController if the URI starts with /*
-				foreach($routerConfigs as $module) {
-					$router->addResource( ucfirst($module), '/' . lcfirst($module) );
-				}
-
-				return $router;
-		});
 
 		/**
 		 * Database connection is created based in the parameters defined in the configuration file
 		 */
 		$di->setShared('db', function () {
-				$config = ConfigLibrary::get('config','database');
+			$config = ConfigLibrary::get('config','database');
 
-				$class = 'Phalcon\Db\Adapter\Pdo\\' . $config->adapter;
-				$params = [
-					'host'     => $config->host,
-					'username' => $config->username,
-					'password' => $config->password,
-					'dbname'   => $config->dbname,
-					'charset'  => $config->charset
-				];
+			$class = 'Phalcon\Db\Adapter\Pdo\\' . $config->adapter;
+			$params = [
+				'host'     => $config->host,
+				'username' => $config->username,
+				'password' => $config->password,
+				'dbname'   => $config->dbname,
+				'charset'  => $config->charset
+			];
 
-				if ($config->adapter == 'Postgresql') {
-					unset($params['charset']);
-				}
+			if ($config->adapter == 'Postgresql') {
+				unset($params['charset']);
+			}
 
-				$connection = new $class($params);
+			$connection = new $class($params);
 
-				return $connection;
+			return $connection;
 		});
 
-		 $di->setShared('basePageInfo',new BasePageInfo());
+		$di->setShared('basePageInfo',new BasePageInfo());
 
 		/**
 		 * Start the session the first time some component request the session service
 		 */
 		$di->setShared('session', function () {
-				$session = new SessionAdapter();
-				$session->start();
+			$session = new SessionAdapter();
+			$session->start();
 
-				return $session;
+			return $session;
 		});
-
 	}
 }
